8dd4e0869f8822b1b533ae027e22a4c9d033dbd9
Leon Landeka <leon.landeka7@gmail.com>
2024-07-04 13:08:29 +0200

    first commit

---
+++ src/components/Stats.jsx
@@ -0,0 +1,21 @@
+export default function Stats({
+  stats
+}) {
+  return (
+    <section className="stats">
+      <Stat number={stats.numberOfWords} label={"Words"}/>
+      <Stat number={stats.numberOfCharacters} label={"Characters"} />
+      <Stat number={stats.instagramCharsLeft} label={"Instagram"}/>
+      <Stat number={stats.facebookCharsLeft} label={"Facebook"}/>
+    </section>
+  );
+}
+
+function Stat({label, number}) {
+  return (
+  <section className="stat"> 
+    <span className={`stat__number ${number < 0 ? 'stat__number--limit' : ''}`}>{number}</span>
+    <h2 className="second-heading">{label}</h2>
+  </section>
+  );
+}
